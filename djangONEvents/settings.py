a5fa54771d910e118ead32bc55d7e3e945da1435
b1z Fox <b1zeefox@gmail.com>
2021-03-02 16:22:55 +0100

    responsive dashboard

--- djangONEvents/settings.py
+++ djangONEvents/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'yb&-nv^jpv2499j632%&3v18#m)25hm+lm-=v*e=i4yqc66y9t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.232','localhost']
 
 AUTH_USER_MODEL = 'accounts.User'
 
